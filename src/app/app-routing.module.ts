ae29b860a3ace491d99739039b13f58bf4e1f72a
Faika123 <faikabenzaara@gmail.com>
2023-02-27 11:03:53 +0100

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'authentification', component: AuthentificationComponent }
 ];
